ba7492927e4420dc75a6628ab8233fbe5fff5336
Mikeresolve <txrz56ad@live.com.mx>
2021-02-10 14:33:27 -0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,31 @@
+const date = (document.getElementById(
+  "date"
+).innerHTML = new Date().getFullYear());
+// ********** nav toggle ************
+// select button and links
+const navBtn = document.getElementById("nav-toggle");
+const links = document.getElementById("nav-links");
+// add event listener
+navBtn.addEventListener("click", () => {
+  links.classList.toggle("show-links");
+});
+
+// ********** smooth scroll ************
+// select links
+const scrollLinks = document.querySelectorAll(".scroll-link");
+
+scrollLinks.forEach(link => {
+  link.addEventListener("click", e => {
+    // prevent default
+    e.preventDefault();
+    const id = e.target.getAttribute("href").slice(1);
+    const element = document.getElementById(id).offsetTop;
+
+    window.scrollTo({
+      left: 0,
+      // top: element.offsetTop,
+      top: element,
+      behavior: "smooth"
+    });
+  });
+});
